bf3afd431581de9ea1202f2c44904cc4c74a10fa
Nicholas Day <nick@tutorialsbynick.com>
2018-09-27 18:07:38 -0400

    Add login and deploy functionality

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_msearch import Search
 from werkzeug import secure_filename
-from flask import send_from_directory
+from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_bcrypt import Bcrypt
 
 import click
 import os
@@ -17,6 +18,7 @@ from pdfminer.pdfpage import PDFPage
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['UPLOAD_FOLDER'] = "resumes"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -24,10 +26,41 @@ migrate = Migrate(app, db)
 search = Search()
 search.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "login"
+
+bcrypt = Bcrypt(app)
+
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
+    password_hash = db.Column(db.String(500))
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 class Hacker(db.Model):
     __tablename__ = 'hacker'
@@ -78,29 +111,60 @@ def create_hacker(name, path):
     db.session.add(hacker)
     db.session.commit()
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        name = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(name=name).first()
+        if user is not None:
+            if user.check_password(password):
+                login_user(user)
+
+        return redirect(url_for("index"))
+    else:
+        return render_template("login.html")
+
 @app.route('/add', methods=['POST'])
+@login_required
 def add_hacker():
     name = request.form['hacker-name']
     uploaded_file = request.files['file-upload']
     filename = secure_filename(uploaded_file.filename)
-    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename)
     uploaded_file.save(path)
 
     resume = convert_pdf_to_txt(path)
     hacker = Hacker(name, filename, resume)
     db.session.add(hacker)
     db.session.commit()
-    return "done"
+    return redirect(url_for("index"))
+
 
 @app.route('/search', methods=['POST', 'GET'])
+@login_required
 def search():
-    results = Hacker.query.msearch(request.form['search'])
+    results = Hacker.query.msearch(request.form['search'], or_=True)
     return render_template('search.html', results=results)
 
 @app.route('/uploads/<filename>')
+@login_required
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+@app.cli.command()
+@click.argument('name')
+@click.argument('password')
+def add_user(name, password):
+    user = User(name, password)
+    db.session.add(user)
+    db.session.commit()
 
 @app.cli.command()
 @click.argument('name')
@@ -114,5 +178,6 @@ def create_index():
     search.create_index(update=True)
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
